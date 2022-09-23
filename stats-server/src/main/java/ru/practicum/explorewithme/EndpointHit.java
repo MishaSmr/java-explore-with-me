5f39f6502e91daac3f8398028f9f75885cc6e1c6
Mik <mikvv90@yandex.ru>
2022-09-23 21:40:30 +0400

    Fix checkstyle

--- stats-server/src/main/java/ru/practicum/explorewithme/EndpointHit.java
+++ stats-server/src/main/java/ru/practicum/explorewithme/EndpointHit.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.time.Instant;
 import java.time.LocalDateTime;
 
 @Entity
--- stats-server/src/main/java/ru/practicum/explorewithme/EndpointHitDto.java
+++ stats-server/src/main/java/ru/practicum/explorewithme/EndpointHitDto.java
@@ -3,8 +3,6 @@ package ru.practicum.explorewithme;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 @AllArgsConstructor
 public class EndpointHitDto {
